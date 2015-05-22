a1425b6fe8438af115ca32463ded27114a9c3f60
Philip Kukulak <philip.kukulak@mail.utoronto.ca>
2015-05-22 18:13:56 -0400

    Put example Sudokus in a different file.

---
+++ SudokuExamples.hs
@@ -0,0 +1,26 @@
+module SudokuExamples where
+
+import Data.Char
+import Data.List
+import Data.Maybe
+import Data.List.Split
+import System.Random
+
+example1 :: Sudoku
+example1 = Sudoku
+      [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+      , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+      , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+      , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+      , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+      , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+      , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+      , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+      , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+      ]
+
+invalidExample :: Sudoku
+invalidExample = Sudoku
+    [ [Just 3, Just 6, Nothing]
+    , [Nothing, Nothing]
+    ]
